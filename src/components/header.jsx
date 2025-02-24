6df8cbe5fd3877ae82918e0f1b926855bbb80e71
santanaIgot <santanaigoor01@gmail.com>
2025-02-24 16:40:23 -0300

    [feat] - component header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import styles from'./header.module.css'
 
 import igniteLogo from '../assets/Ignite simbol.svg'
 
-export function Header(params) {
+export function Header() {
     return(
         <header className={styles.header}>
             <img src={igniteLogo}/>
