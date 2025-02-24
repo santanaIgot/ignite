f94397f21f3cfd5bc162a3aa0745910c453e6df0
santanaIgot <santanaigoor01@gmail.com>
2025-02-24 16:41:37 -0300

    [feat] - component post

---
+++ src/components/post.jsx
@@ -0,0 +1,26 @@
+import styles from './post.module.css'
+
+export function Post() {
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/santanaIgot.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Igor Ferreira Santana</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='11 de maio as 08:13:14' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p> <a href=''>👉 jane.design/doctorcare</a></p>
+                <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+                
+            </div>
+        </article>
+    )
+}
