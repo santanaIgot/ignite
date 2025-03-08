6a426ac88fe7c163abe39b55d027f266d2b2e891
santanaIgot <santanaigoor01@gmail.com>
2025-03-07 23:05:16 -0300

    [feat] - componente avatar

---
+++ src/components/avatar.jsx
@@ -0,0 +1,13 @@
+import styles from './avatar.module.css'
+
+export function Avatar({hasBorder = true , src }){
+
+   
+
+    return(
+        <img 
+            className={hasBorder ? styles.avatarWithBorder : styles.avatar}  
+            src={src} 
+        />
+    )
+}
