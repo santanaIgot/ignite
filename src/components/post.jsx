535e4a3c17b97b09538a3e9017ec672935e5449f
santanaIgot <santanaigoor01@gmail.com>
2025-03-09 21:53:31 -0300

    [feat] - desestruturação de propriedades e percorrendo array de objeto

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,31 +1,45 @@
+import {format, formatDistanceToNow} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './avatar'
 import {Comment} from './comment'
 import styles from './post.module.css'
 
  
 
-export function Post(props) {
+export function Post({author, publishedAt, content}) {
 
-    console.log(props);
+    const publishedDateFormatted = format(publishedAt,"d 'de' LLLL 'as' HH:mm'h'", {
+        locale: ptBR,
+    } )
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix:true
+    })
     
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar  src="https://github.com/santanaIgot.png" />
+                    <Avatar  src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Igor Ferreira Santana</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title='11 de maio as 08:13:14' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}            
+                </time>
             </header>
             <div className={styles.content}>
                 
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p> <a href=''>👉 jane.design/doctorcare</a></p>
-                <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+               {content.map(line => {
+                if(line.type === 'paragraph'){
+                    return <p>{line.content}</p>
+                }else if(line.type === 'link'){
+                    return <p><a href="#">{line.content}</a></p>
+                }
+               })}
                 
             </div>
             {/* COMENTÁRIO */}
